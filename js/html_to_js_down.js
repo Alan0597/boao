d575f52c4ec9e740d8d4cbba5cc7091a68f34800
Alan0597 <66154776+Alan0597@users.noreply.github.com>
2022-05-07 21:52:43 +0800

    Update html_to_js_down.js

--- js/html_to_js_down.js
+++ js/html_to_js_down.js
@@ -5,7 +5,7 @@ document.writeln("			<div id=\'lxi\'>");
 document.writeln("				<a href=\'#\'>联系我们</a> | <a href=\'#\'>法律声明</a> | <a href=\'#\'>下载中心</a> | <a href=\'#\'>邮箱登入</a>");
 document.writeln("			</div>");
 document.writeln("			<div id=\'copy\'>");
-document.writeln("				<p>Alan的大二作业作品&nbsp;&nbsp;&nbsp;&nbsp;博鳌亚洲论坛版权所有&nbsp;&nbsp;&nbsp;&nbsp;琼ICP备案17004813号</p>");
+document.writeln("				<p>alan的大二课时作业&nbsp;&nbsp;&nbsp;&nbsp;博鳌亚洲论坛版权所有&nbsp;&nbsp;&nbsp;&nbsp;琼ICP备案17004813号</p>");
 document.writeln("				<p>Copyright &copy;2001-2019 BFA All Rights Reserved&nbsp;&nbsp;&nbsp;&nbsp;京公网备案 11010502034126号</p>");
 document.writeln("			</div>");
 document.writeln("			<div id=\'erwm\'>");
